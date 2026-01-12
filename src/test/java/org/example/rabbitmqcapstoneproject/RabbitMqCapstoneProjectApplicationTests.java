059bf3b022d473699ba5a745604e7ceef04a104f
aziolkowska <aziolkowska@griddynamics.com>
2026-01-12 11:21:45 +0100

    initial commit

---
+++ src/test/java/org/example/rabbitmqcapstoneproject/RabbitMqCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rabbitmqcapstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqCapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
